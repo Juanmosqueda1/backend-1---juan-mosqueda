5c0b42f26c9c6ebad386b563cd44d18ca7c0da72
juan mosqueda <j.ignaciomosqueda@gmail.com>
2024-07-24 15:47:06 -0300

    preentrega 1 terminada

--- src/ROUTES/products.router.js
+++ src/ROUTES/products.router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const productManager = require("../managers/product-managers.js");
+const e = require("express");
 const manager = new productManager("./src/data/productos.json");
 
 //listar los productos
@@ -15,7 +16,7 @@ router.get("/", async (req, res) => {
 router.get("/:pid", async (req, res) => {
   let id = req.params.pid;
   try {
-    const producto = await manager.getProductById(parseInt(id));
+    const producto = await manager.getProductById(id);
 
     if (!producto) {
       res.send("producto no encontrado");
@@ -24,6 +25,42 @@ router.get("/:pid", async (req, res) => {
     }
   } catch (error) {
     res.send("error al buscar ese id en los productos");
+  }
+});
+
+router.post("/", async (req, res) => {
+  const nuevoProducto = req.body;
+
+  try {
+    await manager.addProduct(nuevoProducto);
+
+    res.status(201).send("Producto agregado exitosamente");
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+});
+
+router.put("/:pid", async (req, res) => {
+  let id = req.params.pid;
+  let product = req.body;
+
+  try {
+    await manager.updateProduct(id, product);
+    res.status(200).send("Producto actualizado");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error al actualizar el producto");
+  }
+});
+
+router.delete("/:pid", async (req, res) => {
+  let id = req.params.pid;
+  try {
+    await manager.deleteProduct(id);
+    res.status(200).send("Producto eliminado con éxito");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error al eliminar el producto");
   }
 });
 
